9e27fa29f4d7fe014114f9b8c6869076a5ca49d7
Andy Conway <aconway01@gmail.com>
2017-08-31 14:12:40 -0400

    Added Percent Diff for Stirling

--- StirlingFormula.py
+++ StirlingFormula.py
@@ -15,8 +15,9 @@ def main():
     stirling_arr = []
     x_arr = []
     diff_arr = []
+    percent_diff_arr = []
 
-    print("n","    ","fact(n)","        ","stirling(n)","        ","fact(n) - stirling(n)")
+    print("n","    ","fact(n)","        ","stirling(n)","    ","fact(n) - stirling(n)","    ","Percent Difference")
     print("------------------------------------------------------------------------------")
 
     for i in range(1,11): # for 30 values [1,...,10]
@@ -24,22 +25,28 @@ def main():
         fact_arr.append(fact(i))
         stirling_arr.append(stirling(i))
         diff_arr.append(round(fact_arr[i-1] - stirling_arr[i-1],5))
-        print(i,"    ",fact_arr[i-1],"            ",stirling_arr[i-1],"        ",diff_arr[i-1])
+        percent_diff_arr.append(round((fact_arr[i-1]-stirling_arr[i-1])/((fact_arr[i-1]+stirling_arr[i-1])/2),3))
+        print(i,"    ",fact_arr[i-1],"            ",stirling_arr[i-1],"        ",diff_arr[i-1],"        ",percent_diff_arr[i-1])
 
 
     # compose plot
+
     '''
     plt.plot(x_arr,fact_arr,'bo',x_arr,stirling_arr,'r^')
     plt.title('Factorial (blue) vs Stirling Formula (red)')
     plt.ylabel('x(n)')
     plt.xlabel('n')
-    '''
 
     plt.plot(x_arr,diff_arr,'b--')
     plt.title('fact(n) - stirling(n)')
     plt.ylabel('difference')
     plt.xlabel('n')
+    '''
 
+    plt.plot(x_arr,percent_diff_arr,'b--')
+    plt.title('Percent Difference')
+    plt.ylabel('Percent')
+    plt.xlabel('n')
 
     plt.show() # show the plot
 
